7fcffcfb30ed80d05585012952f7ddc9fb44e3b1
dkublik <dkublik@gmail.com>
2018-03-18 20:04:41 +0100

    example contract specs

---
+++ mocks/src/test/java/contract/BooksContractVerifierTest.java
@@ -0,0 +1,36 @@
+package contract;
+
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.springframework.http.HttpStatus.ACCEPTED;
+import static pl.dk.soa.service.Hosts.MOCK_HOST;
+
+public class BooksContractVerifierTest {
+
+	static final String HOST = MOCK_HOST;
+
+	@Test
+	public void validateEcuAccPedalPosition() {
+		// given:
+			RequestSpecification request = given()
+                    .contentType(JSON)
+                    .body(new JSONObject()
+                            .put("title", "Ogniem i Mieczem")
+							.put("author", "Sienkiewicz")
+                            .toString()
+                    );
+		// when:
+			Response response = request.when()
+					.post(HOST + "/library/books");
+
+		// then:
+			response.then()
+					.statusCode(ACCEPTED.value())
+					.contentType(JSON);
+	}
+}
---
+++ mocks/src/test/java/contract/GpsContractVerifierTest.java
@@ -0,0 +1,32 @@
+package contract;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.springframework.http.HttpStatus.OK;
+import static pl.dk.soa.service.Hosts.MOCK_HOST;
+
+public class GpsContractVerifierTest {
+
+	static final String HOST = MOCK_HOST;
+
+	@Test
+	public void validateGps() {
+		// when:
+		Response response = when()
+				.get(HOST + "/gps/my-coordinates");
+
+		// then:
+		response.then()
+				.statusCode(OK.value())
+				.contentType(JSON)
+				.body("lat", allOf(greaterThanOrEqualTo(-180f), lessThanOrEqualTo(180f)))
+				.body("lng", allOf(greaterThanOrEqualTo(-180f), lessThanOrEqualTo(180f)));
+	}
+
+}
